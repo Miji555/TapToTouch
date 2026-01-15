768e02387dbcb45b6914bae78b45a454aa1daacd
Miji555 <147109321+Miji555@users.noreply.github.com>
2026-01-16 02:39:36 +0700

    Update ui.js

--- src/ui.js
+++ src/ui.js
@@ -9,13 +9,4 @@ export function updateUI({ hp, score, combo }) {
   UI.score.innerText = score;
   UI.combo.innerText = combo + 'x COMBO';
 }
-<div id="hud">
-  <div id="hp-container">
-    <div id="hp"></div>
-  </div>
-  <div>
-    <h1 id="score">0</h1>
-    <p id="combo">0x COMBO</p>
-  </div>
-  <button id="pauseBtn">⏸</button>
-</div>
+
