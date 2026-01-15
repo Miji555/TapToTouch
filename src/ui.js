c3dd805c7b07d9bc46f86b0391722d3821ada9b4
Miji555 <147109321+Miji555@users.noreply.github.com>
2026-01-16 02:35:26 +0700

    Update ui.js

--- src/ui.js
+++ src/ui.js
@@ -1,26 +1,11 @@
 export const UI = {
-  menu: document.getElementById('menu'),
-  hud: document.getElementById('hud'),
-  pause: document.getElementById('pause'),
-  gameover: document.getElementById('gameover'),
-
-  scoreText: document.getElementById('scoreText'),
-  finalScore: document.getElementById('finalScore'),
-  bestScore: document.getElementById('bestScore'),
-
-  startBtn: document.getElementById('startBtn'),
-  pauseBtn: document.getElementById('pauseBtn'),
-  resumeBtn: document.getElementById('resumeBtn'),
-  retryBtn: document.getElementById('retryBtn'),
-  homeBtn: document.getElementById('homeBtn'),
-  homeBtn2: document.getElementById('homeBtn2'),
+  hpBar: document.getElementById('hp'),
+  score: document.getElementById('score'),
+  combo: document.getElementById('combo'),
 };
 
-export function show(screen) {
-  UI.menu.classList.add('hidden');
-  UI.hud.classList.add('hidden');
-  UI.pause.classList.add('hidden');
-  UI.gameover.classList.add('hidden');
-
-  UI[screen].classList.remove('hidden');
+export function updateUI({ hp, score, combo }) {
+  UI.hpBar.style.width = hp + '%';
+  UI.score.innerText = score;
+  UI.combo.innerText = combo + 'x COMBO';
 }
